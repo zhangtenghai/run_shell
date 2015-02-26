72839a067c47458f5003a973b57ad3d16b805911
bruce <zth5906@gmail.com>
2015-02-26 15:05:53 +0800

    add spaces

--- lib/run_shell/shell.rb
+++ lib/run_shell/shell.rb
@@ -6,14 +6,14 @@ module RunShell
         if File.exists?(RunShell.config.shell_path)
           content = File.read(RunShell.config.shell_path)
           result = system content
-          shell = Shell.new(file_path:RunShell.config.shell_path, content:content,result:result,whodunnit:RunShell.whodunnit)
+          shell = Shell.new(file_path:RunShell.config.shell_path, content:content, result:result, whodunnit:RunShell.whodunnit)
           shell.save
         end
 
         if !RunShell.config.shell_command.blank?
           content = RunShell.config.shell_command
           result = system content
-          shell = Shell.new(content:content,result:result,whodunnit:RunShell.whodunnit)
+          shell = Shell.new(content:content, result:result, whodunnit:RunShell.whodunnit)
           shell.save
         end
     end
